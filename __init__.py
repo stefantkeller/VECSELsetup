aaaa0c23941c8fcf13bf0dfb2b303a3e51bcaf43
Stefan Keller <kestefan@student.ethz.ch>
2015-02-24 17:39:25 +0100

    error handling in import, enable selective useage of meas or eval on computers that dont use the other (and thus dont have all the modules to import)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
-import eval
+import warnings
 
-import meas
+try:
+    import eval
+except ImportError, e:
+    msg = ''.join([str(e),'; without this you cannot perform evaluation (measurements are not affected)'])
+    warnings.warn(msg, ImportWarning)
+
+try:
+    import meas
+except ImportError, e:
+    msg = ''.join([str(e),'; without this you cannot perform measurements (evaluation is not affected)'])
+    warnings.warn(msg, ImportWarning)
